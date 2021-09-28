549527c8f0d763aaa39639ec25101edcc6020671
Jacob McGuire <dev.jmcguire@gmail.com>
2021-09-28 14:47:13 -0400

    more animation changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ class App extends Component {
     render() {
         return (
             <div id="app">
-                <h1>Welcome</h1>
-                <h4>This is a React Application</h4>
                 <Name/>
             </div>
         );
--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -4,22 +4,44 @@ import { motion } from "framer-motion";
 const { hello, jmcguiredev } = require("../paths.json");
 const TextToSVG = require("text-to-svg");
 
-const icon = {
-  hidden: {
+let nameSize = 0.75;
+
+const nameVariants = {
+  start: {
     pathLength: 0,
     fill: "rgba(255, 255, 255, 0)",
     stroke: "rgb(243, 0, 162)",
   },
-  visible: {
+  finish: {
     pathLength: 1,
     fill: "rgba(255, 255, 255, 0)",
-    stroke: "rgb(21, 255, 0)",
+    stroke: "rgb(250, 0, 229)",
   },
 };
 
+const containerVariants = {
+    start: {
+        y: -800,
+        background: "rgb(50, 16, 56, 0)",
+    },
+    finish: {
+        y: -50,
+        background: "rgb(17, 16, 56, 1)",
+    }
+}
+
 const Name = () => {
   return (
-    <motion.div className="container">
+    <motion.div 
+        className="container" 
+        style={{ transform: `scale(${nameSize})`}} 
+        variants={containerVariants}
+        initial="start"
+        animate="finish"
+        transition={{ 
+            y: { duration: 2, ease: "easeOut" },
+            background: { duration: 3}
+            }}>
       <motion.svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="175 -50 200 200"
@@ -27,12 +49,12 @@ const Name = () => {
       >
         <motion.path
           d={jmcguiredev}
-          variants={icon}
-          initial="hidden"
-          animate="visible"
+          variants={nameVariants}
+          initial="start"
+          animate="finish"
           transition={{
             stroke: { duration: 3 },
-            pathLength: { duration: 10}
+            pathLength: { duration: 15}
           }}
         />
       </motion.svg>
