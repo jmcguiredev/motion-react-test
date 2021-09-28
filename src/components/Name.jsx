07aa56e2bf2dd1be92bb4f77e5162055e73eea08
Jacob McGuire <dev.jmcguire@gmail.com>
2021-09-28 16:18:32 -0400

    fixed animation scaling

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -4,18 +4,19 @@ import { motion } from "framer-motion";
 const { hello, jmcguiredev } = require("../paths.json");
 const TextToSVG = require("text-to-svg");
 
-let nameSize = 0.75;
+const nameSizeStart = 0.3;
+const nameSizeEnd = 1;
 
 const nameVariants = {
   start: {
     pathLength: 0,
     fill: "rgba(255, 255, 255, 0)",
-    stroke: "rgb(243, 0, 162)",
+    stroke: "rgb(243, 0, 162, 0)",
   },
   finish: {
     pathLength: 1,
     fill: "rgba(255, 255, 255, 0)",
-    stroke: "rgb(250, 0, 229)",
+    stroke: "rgb(250, 0, 229, 1)",
   },
 };
 
@@ -23,10 +24,12 @@ const containerVariants = {
     start: {
         y: -800,
         background: "rgb(50, 16, 56, 0)",
+        scale: nameSizeStart,
     },
     finish: {
         y: -50,
         background: "rgb(17, 16, 56, 1)",
+        scale: nameSizeEnd,
     }
 }
 
@@ -34,13 +37,13 @@ const Name = () => {
   return (
     <motion.div 
         className="container" 
-        style={{ transform: `scale(${nameSize})`}} 
         variants={containerVariants}
         initial="start"
         animate="finish"
         transition={{ 
             y: { duration: 2, ease: "easeOut" },
-            background: { duration: 3}
+            background: { duration: 3},
+            scale: { duration: 5, ease: "easeOut"}
             }}>
       <motion.svg
         xmlns="http://www.w3.org/2000/svg"
@@ -53,8 +56,8 @@ const Name = () => {
           initial="start"
           animate="finish"
           transition={{
-            stroke: { duration: 3 },
-            pathLength: { duration: 15}
+            stroke: { duration: 3},
+            pathLength: { duration: 15, delay: 1}
           }}
         />
       </motion.svg>
