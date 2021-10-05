2388f9ecd654d4b6cb99e0f2bb7b53fd708ba003
Jacob McGuire <dev.jmcguire@gmail.com>
2021-10-05 14:46:21 -0400

    added reset button with animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Name from "./components/Name";
 import Controls from "./components/Controls";
 
+
 const App = () => {
   return (
     <div id="app">
--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React from "react";
+import { RotateCw } from "react-feather";
+import { motion } from "framer-motion";
 
-const Controls = ({ }) => {
-    return ( 
+const Controls = ({}) => {
+  return (
     <div className="controls">
-        
-    </div> );
-}
- 
+      <motion.div
+        variants={{ start: { transform: "scale(0.9) rotate(0deg)"}, finish: { transform: "scale(1) rotate(180deg)"} }}
+        initial="start"
+        whileHover="finish"
+        transition={{
+          transform: { duration: 0.2 },
+        }}
+      >
+        <RotateCw className="reset-animation" color="white" />
+      </motion.div>{" "}
+    </div>
+  );
+};
+
 export default Controls;
