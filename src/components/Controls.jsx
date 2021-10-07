9a82f42d8e830d5fefbefc01c4d9fde2c108172b
Jacob McGuire <dev.jmcguire@gmail.com>
2021-10-07 15:37:31 -0400

    added hover animation

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -4,27 +4,40 @@ import { motion, useTransform, useAnimation } from "framer-motion";
 
 const Controls = ({}) => {
 
+  const rotateDur = 0.4;
+  const hoverDur = 0.1;
+
   const controls = useAnimation();
 
   const tap = () => {
     controls
       .start({
-        transform: "rotate(360deg)",
-        transition: { duration: 0.4 },
-      })
-      .then(() => {
-        controls.start({
-          transform: "rotate(0deg)",
-          transition: { duration: 0 },
-        });
+        rotate: [0, 360],
+        transition: { duration: rotateDur },
       });
   };
+
+  const hoverOn = () => {
+    controls.start({
+      scale: [1, 1.1],
+      transition: { duration: hoverDur }
+    });
+  }
+
+  const hoverOff = () => {
+    controls.start({
+      scale: [1.1, 1],
+      transition: { duration: hoverDur }
+    })
+  }
 
   return (
     <div className="controls">
       <motion.div
         animate={controls}
         onTap={tap}
+        onHoverStart={hoverOn}
+        onHoverEnd={hoverOff}
       >
         <RotateCw className="reset-animation" color="white" />
       </motion.div>
