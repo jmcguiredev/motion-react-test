e4874960a1414a04f0335b359873e6522f625cf8
Jacob McGuire <dev.jmcguire@gmail.com>
2021-10-13 14:03:19 -0400

    added shared layout animation to NavLinks

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,32 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { motion, AnimateSharedLayout } from "framer-motion";
-import NavLink from '../NavLink/NavLink';
-import './navbar.scss'
+import NavLink from "../NavLink/NavLink";
+import "./navbar.scss";
 
 const NavBar = () => {
+  const links = [
+    {
+      text: "Home",
+      link: "#",
+    },
+    {
+      text: "About Us",
+      link: "#",
+    },
+    {
+      text: "Contact",
+      link: "#",
+    },
+  ];
 
-    const links = [
-        {
-            text: "Home",
-             link:"#"
-        },
-        {
-            text: "About Us",
-             link:"#"
-        },
-        {
-            text: "Contact",
-             link:"#"
-        }
-    ];
+  const [selected, setSelected] = useState(links[0].text);
 
-    return ( 
-        <motion.nav className="navbar">
-            {links.map((link) => (
-                <NavLink text={link.text} link={link.link}/>
-            ))}
-        </motion.nav>
-     );
-}
- 
+  const handleClick = (text) => {
+    setSelected(text);
+  };
+
+  useEffect(() => {
+    console.log(selected);
+  });
+
+  return (
+    <AnimateSharedLayout>
+      <motion.nav className="navbar">
+        {links.map((link) => (
+          <NavLink
+            key={link.text}
+            text={link.text}
+            link={link.link}
+            onClick={() => setSelected(link.text)}
+            isSelected={selected === link.text}
+          />
+        ))}
+      </motion.nav>
+      </AnimateSharedLayout>
+  );
+};
+
 export default NavBar;
--- src/components/NavLink/NavLink.jsx
+++ src/components/NavLink/NavLink.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React from "react";
 import { motion } from "framer-motion";
 import "./navLink.scss";
 
-const NavLink = ({ text, link }) => {
-    return ( 
-        <motion.a className="navbar-link" href={link}>
-            {text}
-        </motion.a>
-     );
-}
- 
+const NavLink = ({ text, link, isSelected, onClick }) => {
+  return (
+    <motion.div className="navbar-link-container">
+      <motion.a
+        className="navbar-link"
+        href={link}
+        style={
+          isSelected
+            ? {
+                color: "rgb(0, 197, 148)",
+              }
+            : null
+        }
+        onClick={onClick}
+      >
+        {text}
+      </motion.a>
+      {isSelected && (
+        <motion.div layoutId="underline" className="underline"></motion.div>
+      )}
+    </motion.div>
+  );
+};
+
 export default NavLink;
