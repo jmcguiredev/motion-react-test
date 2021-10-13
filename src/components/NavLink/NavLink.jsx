ccfff63e632143941004a3b010ff911890ad66d3
Jacob McGuire <dev.jmcguire@gmail.com>
2021-10-13 14:59:21 -0400

    fixed hover animation on NavLink

--- src/components/NavLink/NavLink.jsx
+++ src/components/NavLink/NavLink.jsx
@@ -1,21 +1,45 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
 import "./navLink.scss";
 
 const NavLink = ({ text, link, isSelected, onClick }) => {
+  
+
+  useEffect(() => {
+    aControl.start({
+        color: isSelected ? "rgb(0, 197, 148)" : "#fff"
+      });
+  })
+
+  const aControl = useAnimation();
+  const hoverDur = 0.33;
+
+  const hoverOn = () => {
+    if (!isSelected) {
+      aControl.start({
+        color: ["#fff", "rgb(0, 197, 148)"],
+        transition: { duration: hoverDur },
+      });
+    }
+  };
+  const hoverOff = () => {
+    if (!isSelected) {
+      aControl.start({
+        color: ["rgb(0, 197, 148)", "#fff"],
+        transition: { duration: hoverDur },
+      });
+    }
+  };
+
   return (
     <motion.div className="navbar-link-container">
       <motion.a
         className="navbar-link"
+        animate={aControl}
         href={link}
-        style={
-          isSelected
-            ? {
-                color: "rgb(0, 197, 148)",
-              }
-            : null
-        }
         onClick={onClick}
+        onHoverStart={hoverOn}
+        onHoverEnd={hoverOff}
       >
         {text}
       </motion.a>
