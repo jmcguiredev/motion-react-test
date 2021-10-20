d2343dd863b5473c99a71912cd675d26c54001bc
Jacob McGuire <dev.jmcguire@gmail.com>
2021-10-20 16:26:59 -0400

    needs fxing

--- src/components/HamburgerMenu/HamburgerMenu.jsx
+++ src/components/HamburgerMenu/HamburgerMenu.jsx
@@ -4,57 +4,61 @@ import "./style";
 
 const HamburgerMenu = ({ refresh }) => {
   const [isOpen, setOpen] = useState(false);
-
   const menuControls = useAnimation();
-
-  
-    // menuControls.start({
-    //   width: 0,
-    //   height: 0,
-    //   borderBottomRightRadius: "5rem"
-    // });
-  
-
-  // refresh ? null : reset();
-
   const menuWidth = "15rem";
+  const duration = 3;
 
-  
-
-  
+  const variants = {
+    open: {
+      width: menuWidth,
+      height: "100%",
+      borderRadius: "0rem",
+    },
+    closed: {
+      width: "3rem",
+      height: "3rem",
+      borderRadius: "100rem",
+      margin: "1rem 0rem 0rem 1rem",
+    },
+  };
 
   const handleClick = () => {
-
-    if(!isOpen) {
-      menuControls.start({
-        width: ["0rem", menuWidth],
-        height: ["0%", "100%"],
-        borderBottomRightRadius: ["5rem", "0rem"],
-        transition: { duration: 3 }
-      });
-    } else {
-      menuControls.start({
-        width: [menuWidth, "0rem"],
-        height: ["100%", "0%"],
-        borderBottomRightRadius: ["0rem", "5rem"],
-        transition: { duration: 3 }
-      });
-    }
+    // if (!isOpen) {
+    //   menuControls.start({
+    //     width: ["0rem", menuWidth],
+    //     height: ["0%", "100%"],
+    //     borderBottomRightRadius: ["5rem", "0rem"],
+    //     transition: { duration: duration },
+    //   });
+    // } else {
+    //   menuControls.start({
+    //     width: [menuWidth, "0rem"],
+    //     height: ["100%", "0%"],
+    //     borderBottomRightRadius: ["0rem", "5rem"],
+    //     transition: { duration: duration },
+    //   });
+    // }
 
     setOpen(!isOpen);
-    
   };
 
   return (
     <motion.div className="hamburger-menu-cont">
-      <motion.div className="hamburger-menu" animate={menuControls} >
-        
-      </motion.div>
+      <motion.div
+        className="hamburger-menu"
+        animate={isOpen ? "open" : "closed"}
+        variants={variants}
+        transition={{
+          width: { duration: 1, ease: "linear" },
+          height: { duration: 1, ease: "linear" },
+          borderRadius: { duration: 1, ease: "linear" },
+        }}
+      ></motion.div>
       <motion.div className="hamburger-menu-button" onClick={handleClick}>
-          <motion.span className="button-stripe" id="button-stripe-2" />
-          <motion.span className="button-stripe" id="button-stripe-1" />
-          <motion.span className="button-stripe" id="button-stripe-3" />
-        </motion.div>
+        <motion.span className="button-stripe" id="button-stripe-2" />
+        <motion.span className="button-stripe" id="button-stripe-1" />
+        <motion.span className="button-stripe" id="button-stripe-3" />
+      </motion.div>
     </motion.div>
   );
 };
